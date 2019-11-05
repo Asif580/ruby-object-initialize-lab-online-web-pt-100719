e93910096c98490a5435b4fe3a3820e3b1ceda81
Asiful Choudhury <asiful307@gmail.com>
2019-11-05 23:36:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,16 @@
 class Dog 
   def initialize(name, breed)
+    @name = name
     @breed = breed
-    @n
   end
+
+ def name=name
+   @name = name
+ end
+ 
+ def name
+   @name 
+ end
  
   def breed=(breed)
     @breed = breed
